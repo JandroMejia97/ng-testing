2e4692d5b7e7415bac82015bb9e10617b7ccfae0
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2022-12-19 20:46:07 -0300

    List all products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '@models/product.model';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [];
 
-  constructor() { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
+  getProducts(): void {
+    this.productService.getAll().subscribe({
+      next: (products) => {
+        this.products = products;
+      },
+    });
+  }
 }
