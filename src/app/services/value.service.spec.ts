191b5fbf40fa9a897872e060d89a78ddf14c9d9f
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2022-12-19 13:42:06 -0300

    Testing services

---
+++ src/app/services/value.service.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+
+import { ValueService } from './value.service';
+
+fdescribe('ValueService', () => {
+  let service: ValueService;
+  const testValue = 'Testing'
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ValueService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('Tests for getter', () => {
+    it(`should return a value distinct of "${testValue}"`, () => {
+      expect(service.value).not.toBe(testValue);
+    });
+
+    it(`should return a value equal to "Example"`, () => {
+      expect(service.value).toBe(service.value);
+    });
+
+  });
+
+  describe('Tests for setter', () => {
+    it(`should change the value and return "${testValue}"`, () => {
+      expect(service.value).not.toBe(testValue);
+      service.value = testValue;
+      expect(service.value).toBe(testValue);
+    });
+  });
+
+  describe('Test for promise', () => {
+    it(`should resolve the promise and return "Example", using "waitForAsync"`, waitForAsync(() => {
+      service.promiseValue.then((value) => {
+        expect(value).toBe(service.value);
+      })
+    }));
+  });
+
+  describe('Test for observables', () => {
+    it(`should use an observable and return "Example", using "waitForAsync"`, waitForAsync(() => {
+      service.observableValue.subscribe({
+        next: (value) => {
+          expect(value).toBe(service.value);
+        }
+      });
+    }));
+  });
+});
---
+++ src/app/services/value.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValueService {
+  #myValue = 'Example';
+
+  constructor() { }
+
+  get value(): string {
+    return this.#myValue;
+  }
+
+  set value(value: string) {
+    this.#myValue = value;
+  }
+
+  get promiseValue(): Promise<string> {
+    return Promise.resolve(this.#myValue)
+  }
+
+  get observableValue(): Observable<string> {
+    return of(this.#myValue)
+  }
+}
