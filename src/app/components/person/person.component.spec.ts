e0b82548e64cf3c76a1abc73199c8741cbc8296d
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2022-12-26 17:53:10 -0300

    Using DebugElement and By.css

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PersonComponent } from './person.component';
 
@@ -20,9 +22,23 @@ describe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have a paragraph with the text "Hello, World!"', () => {
+  it('should have a paragraph with the text "Hello, World!" (using the Native API)', () => {
     const compiled = fixture.nativeElement;
     const p: HTMLElement = compiled.querySelector('p');
     expect(p.textContent?.trim().toLocaleLowerCase()).toContain('hello, world!');
+  });
+
+  it('should have a paragraph with the text "Hello, World!" (using the Debug API)', () => {
+    const debugElement: DebugElement = fixture.debugElement;
+    const compiled = debugElement.nativeElement;
+    const p: HTMLElement = compiled.querySelector('p');
+    expect(p.textContent?.trim().toLocaleLowerCase()).toContain('hello, world!');
+  });
+
+  it('should have a paragraph with the text "Hello, World!" (using the Debug API and By.css)', () => {
+    const debugElement = fixture.debugElement;
+    const pDebugElement: DebugElement  = debugElement.query(By.css('p'));
+    const pNativeElement: HTMLElement = pDebugElement.nativeElement;
+    expect(pNativeElement.textContent?.trim().toLocaleLowerCase()).toContain('hello, world!');
   });
 });
