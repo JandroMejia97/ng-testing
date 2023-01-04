fb7ed283329c1aa3bad3aeebc5f69430314c35fc
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2023-01-04 11:26:24 -0300

    Testing with a DOM element

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -4,6 +4,29 @@ import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
 import { HighlightDirective } from './highlight.directive';
 
+describe('HighlightDirective', () => {
+  let directive: HighlightDirective;
+  let element: HTMLElement;
+
+  beforeEach(() => {
+    element = document.createElement('div');
+    directive = new HighlightDirective({ nativeElement: element });
+  });
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+
+  it('should have the default color when it isn\'t defined', () => {
+    expect(element.style.backgroundColor).toBe(directive.defaultColor);
+  });
+
+  it('should have the color defined', () => {
+    directive.highlight = 'yellow';
+    expect(element.style.backgroundColor).toBe('yellow');
+  });
+});
+
 @Component({
   template: `
     <p highlight>
@@ -21,19 +44,18 @@ import { HighlightDirective } from './highlight.directive';
     </p>
   `,
 })
-export class HostComponent {
+class HostComponent {
   color = 'pink';
 }
 
-describe('HighlightDirective', () => {
+describe('HighlightDirective with HostComponent', () => {
   let hostComponent: HostComponent;
   let fixture: ComponentFixture<HostComponent>;
-  let highlightDirective: HighlightDirective;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HostComponent, HighlightDirective],
       imports: [FormsModule],
+      declarations: [HighlightDirective, HostComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HostComponent);
@@ -45,12 +67,7 @@ describe('HighlightDirective', () => {
     expect(hostComponent).toBeTruthy();
   });
 
-  /* it('should create an instance', () => {
-    const directive = new HighlightDirective();
-    expect(directive).toBeTruthy();
-  }); */
-
-  it('should have 2 elements with the highlight directive and one whitout it', () => {
+  it('should have 3 elements with the highlight directive and 2 whitout it', () => {
     const elements = fixture.debugElement.queryAll(By.directive(HighlightDirective));
     const elementsWithoutDirective = fixture.debugElement.queryAll(By.css(':not([highlight])'));
 
