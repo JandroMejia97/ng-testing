bf13914d36700d24a121bd81b82701ee6a78a5bd
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2023-01-04 21:28:36 -0300

    Adding an event dispatcher

--- src/testing/events.ts
+++ src/testing/events.ts
@@ -8,6 +8,7 @@ import { query, queryByTestId } from './finders';
  *
  * @param fixture The fixture of the component
  * @param selector The selector of the element
+ * @param withTestId Whether to use the test id or not
  * @param eventName The name of the event
  * @param eventObj The event object
  */
@@ -38,4 +39,47 @@ export function triggerClickEventHandler(
   selector: string
 ): void {
   triggerEventHandler(fixture, selector, 'click');
+}
+
+/**
+ * Triggers an event on an element
+ *
+ * @param fixture The fixture of the component
+ * @param selector The selector of the element
+ * @param withTestId Whether to use the test id or not
+ * @param eventName The name of the event
+ * @param eventObj The event object
+ */
+export function triggerEventOnElement(
+  fixture: ComponentFixture<unknown>,
+  selector: string,
+  eventName: string,
+  withTestId = false,
+  eventObj?: EventInit
+): void {
+  let debugElement: DebugElement;
+  if (withTestId) {
+    debugElement = queryByTestId(fixture, selector);
+  } else {
+    debugElement = query(fixture, selector);
+  }
+  const element: HTMLElement = debugElement.nativeElement;
+  element.dispatchEvent(new Event(eventName, eventObj));
+}
+
+/**
+ * Triggers a click event on an element
+ *
+ * @param fixture The fixture of the component
+ * @param selector The selector of the element
+ * @param withTestId Whether to use the test id or not
+ * @param eventObj The event object
+ */
+export function triggerClickEventOnElement(
+  fixture: ComponentFixture<unknown>,
+  selector: string,
+  withTestId = false,
+  eventObj?: EventInit
+): void {
+  triggerEventOnElement(fixture, selector, 'click', withTestId, eventObj);
 }
