960a6222d83fe422441e5d41b5f2dc5834bfce17
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2023-01-06 12:55:29 -0300

    Using the ActivatedRouteStub class

--- src/app/products/components/detail/detail.component.spec.ts
+++ src/app/products/components/detail/detail.component.spec.ts
@@ -1,22 +1,31 @@
+import { Location } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute, RouterModule } from '@angular/router';
+import { generateOneProduct } from '@models/mocks/product.mock';
+
 import { ProductService } from '@services/product.service';
+import { ActivatedRouteStub, observableData } from '@testing';
 
 import { DetailComponent } from './detail.component';
 
 describe('DetailComponent', () => {
   let component: DetailComponent;
   let fixture: ComponentFixture<DetailComponent>;
+
   let productServiceSpy: jasmine.SpyObj<ProductService>;
-  let activatedRouteSpy: jasmine.SpyObj<ActivatedRoute>;
+  let activatedRouteSpy: ActivatedRouteStub;
+  let locationSpy: jasmine.SpyObj<Location>;
+
+  const product = generateOneProduct();
+  const productId = product.id;
 
   beforeEach(async () => {
     const productServiceSpyObj = jasmine.createSpyObj('ProductService', [
       'getOne',
     ]);
-    const activatedRouteSpyObj = jasmine.createSpyObj('ActivatedRoute', [], {
-      paramMap: jasmine.createSpyObj('paramMap', ['subscribe']),
-    });
+    const activatedRouteStub = new ActivatedRouteStub();
+    const locationSpyObj = jasmine.createSpyObj('Location', ['back']);
+
     await TestBed.configureTestingModule({
       declarations: [DetailComponent],
       imports: [RouterModule],
@@ -27,21 +36,34 @@ describe('DetailComponent', () => {
         },
         {
           provide: ActivatedRoute,
-          useValue: activatedRouteSpyObj,
+          useValue: activatedRouteStub,
         },
+        {
+          provide: Location,
+          useValue: locationSpyObj,
+        }
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(DetailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
 
+    // Get injected services
     productServiceSpy = TestBed.inject(
       ProductService
     ) as jasmine.SpyObj<ProductService>;
     activatedRouteSpy = TestBed.inject(
       ActivatedRoute
-    ) as jasmine.SpyObj<ActivatedRoute>;
+    ) as unknown as ActivatedRouteStub;
+    locationSpy = TestBed.inject(Location) as jasmine.SpyObj<Location>;
+
+    // Set up spies
+    productServiceSpy.getOne.and.returnValue(
+      observableData(product)
+    );
+    activatedRouteSpy.setParamMap({ id: productId });
+
+    fixture.detectChanges();
   });
 
   it('should create', () => {
